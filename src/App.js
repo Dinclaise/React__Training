1a3bb2260e9df8abbe81ab4c15947462afa44699
Anton <aanohin98@gmail.com>
2019-12-18 20:59:29 +0300

    0.6.0
    //

--- src/App.js
+++ src/App.js
@@ -1,126 +1,59 @@
-import React, { Component } from 'react';
-import cls from './App.module.scss';
-import Car from './Car/Car';
-import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
-import Counter from './Counter/Counter';
-
-
-export const ClickedContext = React.createContext(false)
+import React, {Component} from 'react'
+import './App.scss'
+import {Route, NavLink, Switch} from 'react-router-dom'
+import About from './About/About'
+import Cars from './Cars/Cars'
+import CarDetail from './CarDetail/CarDetail'
 
 class App extends Component {
 
-    constructor(props) {
-      super(props)
-
-      this.state = {
-        clicked: false,
-        cars: [
-          {name: 'Ford', year: 2018},
-          {name: 'Audi', year: 2016},
-          {name: 'Mazda', year: 2010}
-        ],
-        pageTitle: 'React components',
-        showCars: false
-      }
-    }
-
-  toggleCarsHandler = () => {
-    this.setState({
-      showCars: !this.state.showCars
-    })
+  state = {
+    isLoggedIn: false
   }
 
- onChangeName(name, index) {
-    console.log(name, index);
-    const car = this.state.cars[index];
-    car.name = name;
-    const cars = [...this.state.cars];
-    cars[index] = car
-    this.setState({
-      cars: cars
-    })
- }
-
- deleteHandler(index) {
-    const cars = this.state.cars.concat();
-    cars.splice(index, 1);
-    this.setState({
-      cars: cars
-    })
- }
-
- componentWillMount() {
-   console.log('App componentWillMount');
- }
-
- componentDidMount() {
-  console.log('App componentDidMount');
- }
 
   render() {
-    console.log('App render');
-    const divStyle = {
-      textAlign: 'center'
-    }
+
     return (
-      <div style={divStyle}>
-        <h1>{this.state.pageTitle}</h1>
-        
-        <ClickedContext.Provider value={this.state.clicked}>
-          <Counter />
-        </ClickedContext.Provider>
-        
+      <div>
+        <nav className="nav">
+          <ul>
+            <li>
+              <NavLink to="/" exact activeClassName={'wfm-active'}>Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about" activeStyle={{color: 'blue'}}>About</NavLink>
+            </li>
+            <li>
+              <NavLink to={{
+                pathname: '/cars'
+              }}>Cars</NavLink>
+            </li>
+          </ul>
+        </nav>
 
-        <button 
-          style={{marginTop: '10px'}}
-          onClick={this.toggleCarsHandler} 
-          className={cls.AppButton}
-        >Toggle cars</button>
+        <hr/>
+        <div style={{textAlign: 'center'}}>
+              <h3>Is Logged In: {this.state.isLoggedIn ? 'True' : 'False'}</h3>
+              <button onClick={() => this.setState({isLoggedIn: !this.state.isLoggedIn})}>Log In</button>
+        </div>
+        <hr/>
 
-        <button onClick={() => this.setState({clicked: true})}>Change clicked</button>
+        {/*localhost:3000*/}
+        <Switch>
+          <Route path="/" exact render={() => <h1>Home Page</h1>} />
+          { this.state.isLoggedIn ?  
+            <Route path="/about" component={About} /> 
+            : null
+          }
+          <Route path="/cars/:name" component={CarDetail} />
+          <Route path="/cars" component={Cars} />
+          <Route render={() => <h1 style={{color: 'red', textAlign:'center'}}>404 not found</h1>} />
+        </Switch>
 
-        { this.state.showCars
-          ? this.state.cars.map((car, index) => {
-              return(
-                <div key={index}
-                  style={{
-                  width: 400,
-                  margin: 'auto',
-                  paddingTop: '20px'
-                }}>
-                  <ErrorBoundary>
-                  <Car 
-                    name={car.name}
-                    year={car.year}
-                    index={index}
-                    onDelete={this.deleteHandler.bind(this, index)}
-                    onChangeName={event => this.onChangeName(event.target.value, index)}
-                  />
-                  </ErrorBoundary>
-                </div>
-              )
-            })
-          : null
-        }
-
-        {/* <Car 
-          name={cars[0].name} 
-          year={cars[0].year}
-          onChangeTitle={this.changeTitleHandler.bind(this, cars[0].name)}
-        />
-        <Car 
-          name={cars[1].name} 
-          year={cars[1].year}
-          onChangeTitle={() => this.changeTitleHandler(cars[1].name)}
-        />
-        <Car 
-          name={cars[2].name} 
-          year={cars[2].year}
-          onChangeTitle={() => this.changeTitleHandler(cars[2].name)}
-        /> */}
       </div>
     );
   }
 }
 
-export default App;
+export default App
